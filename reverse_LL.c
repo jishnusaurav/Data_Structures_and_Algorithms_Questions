46088ba256d99a97341e1bb5a3d4093a4f0c4ece
Jishnu Saurav Mittapalli <55013611+jishnusaurav@users.noreply.github.com>
2021-03-25 01:32:28 +0530

    -

---
+++ reverse_LL.c
@@ -0,0 +1,15 @@
+  Node* current = head;
+        Node *prev = NULL, *next = NULL;
+ 
+        while (current != NULL) {
+            // Store next
+            next = current->next;
+ 
+            // Reverse current node's pointer
+            current->next = prev;
+ 
+            // Move pointers one position ahead.
+            prev = current;
+            current = next;
+        }
+        head = prev;
