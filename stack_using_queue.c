89710c2e684370d962bfb5f3650269c09b669238
Jishnu Saurav Mittapalli <55013611+jishnusaurav@users.noreply.github.com>
2021-05-04 21:07:24 +0530

    Create stack_using_queue.c

---
+++ stack_using_queue.c
@@ -0,0 +1,22 @@
+push(int x)
+{
+        // Your Code
+        q1.push(x);
+        
+        
+}
+
+//Function to pop an element from stack using two queues. 
+pop()
+{
+        // Your Code     
+        while(!q1.empty()){
+        q2.push(q1.front());
+        q1.pop();
+        }
+        q1.pop();
+        while(!q2.empty()){
+        q1.push(q2.front());
+        q2.pop();
+        }
+}
