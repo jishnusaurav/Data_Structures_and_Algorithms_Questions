e6a11c1a36986b9fed2513ab9721d322927880e7
Jishnu Saurav Mittapalli <55013611+jishnusaurav@users.noreply.github.com>
2021-05-06 01:00:23 +0530

    Companies -  Amazon

---
+++ Maxlength_valid_substring.c
@@ -0,0 +1,31 @@
+    int findMaxLen(string str) {
+            int n = str.length();
+ 
+    stack<int> stk;
+    stk.push(-1);
+ 
+    int result = 0;
+ 
+    for (int i = 0; i < n; i++)
+    {
+    if (str[i] == '(')
+            stk.push(i);
+         
+        else
+        {
+            if (!stk.empty())
+            {
+                stk.pop();
+            }
+             
+            if (!stk.empty())
+                result = max(result, i - stk.top());
+ 
+           
+            else
+                stk.push(i);
+        }
+    }
+ 
+    return result;
+    }
