22c1e6ba7800dc8515a27293fe95166d97b6fbd9
Jishnu Saurav Mittapalli <55013611+jishnusaurav@users.noreply.github.com>
2021-05-07 19:54:48 +0530

    Companies - -

---
+++ Interleave_first_and_second_halfs_of_queue.c
@@ -0,0 +1,47 @@
+void interLeaveQueue(queue<int>& q)
+{
+    
+    if (q.size() % 2 != 0)
+        cout << "Input even number of integers." << endl;
+  
+    stack<int> s;
+    int halfSize = q.size() / 2;
+  
+    // Push first half elements into the stack
+    // queue:16 17 18 19 20, stack: 15(T) 14 13 12 11
+    for (int i = 0; i < halfSize; i++) {
+        s.push(q.front());
+        q.pop();
+    }
+  
+    // enqueue back the stack elements
+    // queue: 16 17 18 19 20 15 14 13 12 11
+    while (!s.empty()) {
+        q.push(s.top());
+        s.pop();
+    }
+  
+    // dequeue the first half elements of queue
+    // and enqueue them back
+    // queue: 15 14 13 12 11 16 17 18 19 20
+    for (int i = 0; i < halfSize; i++) {
+        q.push(q.front());
+        q.pop();
+    }
+  
+    // Again push the first half elements into the stack
+    // queue: 16 17 18 19 20, stack: 11(T) 12 13 14 15
+    for (int i = 0; i < halfSize; i++) {
+        s.push(q.front());
+        q.pop();
+    }
+  
+    // interleave the elements of queue and stack
+    // queue: 11 16 12 17 13 18 14 19 15 20
+    while (!s.empty()) {
+        q.push(s.top());
+        s.pop();
+        q.push(q.front());
+        q.pop();
+    }
+}
